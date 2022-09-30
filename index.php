cf26542135877627c6604ec5161e806f3f12a033
KNN3477 <106166512+KNN3477@users.noreply.github.com>
2022-10-01 01:15:51 +0300

    feat: init
    init html

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php 
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+	<style>
+		* {
+			outline: none;
+			margin: 0;
+			padding: 0;
+		}
+	</style>	
+	<div style="width: 100vw; height: 100vh; background: #fff url(work.png) 50% 50% no-repeat;"></div>
+</body>
+</html>
+?>
